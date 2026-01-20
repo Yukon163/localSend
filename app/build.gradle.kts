269749f08f14dbf6d7d86c96593a6f22396456cb
Yukon163 <18096690703@163.com>
2026-01-20 16:38:06 +0800

    fix(build): apply kotlin-android plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("org.jetbrains.kotlin.android")
     alias(libs.plugins.kotlin.compose)
 }
 
@@ -43,6 +44,14 @@ android {
     }
 
 
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions.jvmTarget = "17"
 }
 
 dependencies {
